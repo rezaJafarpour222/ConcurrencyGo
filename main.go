660a76060bc0009137be8ded4bc9edce97e786d4
reza <rezajafarpoor222@gamil.com>
2026-02-18 17:20:54 +0330

    race problem added

--- main.go
+++ main.go
@@ -1,30 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-func printSomething(s string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println(s)
-}
 func main() {
-	var wg sync.WaitGroup
-	words := []string{
-		"alpha",
-		"beta",
-		"delta",
-		"gamma",
-		"pi",
-		"zeta",
-		"eta",
-		"theta",
-		"epsilon",
-	}
-	wg.Add(len(words))
-	for i, x := range words {
-		printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
-	}
-	wg.Wait()
+	race()
 }
---
+++ rac_condition.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var msg string
+var wg sync.WaitGroup
+
+func updateMessage(s string) {
+	defer wg.Done()
+	msg = s
+}
+
+func race() {
+	msg = "Hello, world"
+	wg.Add(2)
+	go updateMessage("Hello, universe")
+	go updateMessage("Hello, cosmos")
+	wg.Wait()
+	fmt.Println(msg)
+}
