2ccbc894ee6acee29bce77966cc7ba3e41ad8be7
reza <rezajafarpoor222@gamil.com>
2026-02-18 17:24:24 +0330

    mutex

--- rac_condition.go
+++ rac_condition.go
@@ -8,16 +8,19 @@ import (
 var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string) {
+func updateMessage(s string, m *sync.Mutex) {
 	defer wg.Done()
+	m.Lock()
 	msg = s
+	m.Unlock()
 }
 
 func race() {
 	msg = "Hello, world"
+	var mux sync.Mutex
 	wg.Add(2)
-	go updateMessage("Hello, universe")
-	go updateMessage("Hello, cosmos")
+	go updateMessage("Hello, universe", &mux)
+	go updateMessage("Hello, cosmos", &mux)
 	wg.Wait()
 	fmt.Println(msg)
 }
